6dfd8383a63f509deb796086c8cf89cb30960831
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-24 10:51:22 -0400

    3

--- main.js
+++ main.js
@@ -7,10 +7,21 @@ function challengeBanner(num){
     challengeBanner(2)
 
 function print(num){
-let num = 1;
+  num = 1;
     while(num <= 5){
         console.log(num)
             num = num + 1;
     }
 
 }
+print(6)
+
+challengeBanner(3)
+function firstTilLast(num, num2){
+ while(num <= num2){
+     console.log(num)
+ 
+ num = num + 1;
+}
+ }
+firstTilLast(3,7)
